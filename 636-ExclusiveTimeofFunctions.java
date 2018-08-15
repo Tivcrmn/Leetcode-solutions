1bbc5ed70400247ae683a3837f20049cfd2391b9
Tivcrmn <zhangjitao0405@gmail.com>
2018-08-15 10:03:42 +0800

    update 636

--- 636-ExclusiveTimeofFunctions.java
+++ 636-ExclusiveTimeofFunctions.java
@@ -1,51 +1,22 @@
-// not working now...
 class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
         int[] res = new int[n];
-        Stack<String> s = new Stack<>();
-        String prev = null;
+        int pre = 0;
+        Stack<Integer> s = new Stack<>();
         for (String log : logs) {
-            if (s.isEmpty()) s.push(log);
-            else {
-                if (getId(s.peek()) == getId(log)) {
-                    if (getStatus(s.peek()).equals("start") && getStatus(log).equals("end")) {
-                        if (prev == null) {
-                            res[getId(log)] += getTime(log) - getTime(s.peek()) + 1;
-                            s.pop();
-                            prev = log;
-                        } else {
-                            res[getId(log)] += getTime(log) - getTime(prev);
-                            s.pop();
-                            prev = null;
-                        }
-                    } else {
-                        res[getId(log)] += getTime(log) - getTime(s.peek());
-                        s.push(log);
-                    }
-                } else {
-                    if (prev == null) {
-                        res[getId(log)] += getTime(log) - getTime(s.peek());
-                        prev = log;
-                    } else {
-                        res[getId(log)] += getTime(log) - getTime(prev) - 1;
-                        prev = null;
-                    }
-                    s.push(log);
-                }
+            String[] l = log.split(":");
+            int id = Integer.parseInt(l[0]), cur = Integer.parseInt(l[2]);
+            String status = l[1];
+            if (status.equals("start")) {
+                if (!s.isEmpty()) res[s.peek()] += cur - pre;
+                s.push(id);
+                pre = cur;
+            } else {
+                res[s.peek()] += cur - pre + 1;
+                s.pop();
+                pre = cur + 1;
             }
         }
         return res;
-    }
-
-    private int getId(String s) {
-        return Integer.parseInt(s.substring(0, s.indexOf(":")));
-    }
-
-    private int getTime(String s) {
-        return Integer.parseInt(s.substring(s.lastIndexOf(":") + 1));
-    }
-
-    private String getStatus(String s) {
-        return s.substring(s.indexOf(":") + 1, s.lastIndexOf(":"));
     }
 }
